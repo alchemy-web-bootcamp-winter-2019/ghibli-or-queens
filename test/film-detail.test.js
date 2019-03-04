42324f11c8d6e31454693d76b3ae17984d9bf5d7
Payton Lower <paytown@me.com>
2019-03-04 14:34:01 -0800

    test passing for both. need to interpolate

--- test/film-detail.test.js
+++ test/film-detail.test.js
@@ -2,6 +2,40 @@ const test = QUnit.test;
 
 QUnit.module('film detail');
 
+function filmDetailTemplate(film) {
+    const template = document.createElement('template');
+    template.innerHTML = `
+    <tbody>
+        <tr>
+            <th>Title</th>
+            <td>Castle in the Sky</td>
+        </tr>
+        <tr>
+            <th>Description</th>
+            <td>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</td>
+        </tr>
+        <tr>
+            <th>Director</th>
+            <td>Hayao Miyazaki</td>
+        </tr>
+        <tr>
+            <th>Producer</th>
+            <td>Isao Takahata</td>
+        </tr>
+        <tr>
+            <th>Release Date</th>
+            <td>1986</td>
+        </tr>
+        <tr>
+            <th>RT Score</th>
+            <td>95</td>
+        </tr>
+    </tbody>
+    `;
+
+    return template.content;
+}
+
 test('html template for film detail table', assert => {
     // arrange
     const film = {
@@ -15,7 +49,35 @@ test('html template for film detail table', assert => {
     };
 
     // act
+    const result = filmDetailTemplate(film);
 
     // assert
-
+    assert.htmlEqual(result, `
+    <tbody>
+        <tr>
+            <th>Title</th>
+            <td>Castle in the Sky</td>
+        </tr>
+        <tr>
+            <th>Description</th>
+            <td>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</td>
+        </tr>
+        <tr>
+            <th>Director</th>
+            <td>Hayao Miyazaki</td>
+        </tr>
+        <tr>
+            <th>Producer</th>
+            <td>Isao Takahata</td>
+        </tr>
+        <tr>
+            <th>Release Date</th>
+            <td>1986</td>
+        </tr>
+        <tr>
+            <th>RT Score</th>
+            <td>95</td>
+        </tr>
+    </tbody>
+    `);
 });

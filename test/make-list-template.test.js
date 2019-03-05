b28e3b67351457f9d446492f3625acc5ca72fd67
Tommy Tran <tranttommy95@gmail.com>
2019-03-04 16:51:58 -0800

    template tests passed

--- test/make-list-template.test.js
+++ test/make-list-template.test.js
@@ -2,20 +2,20 @@ const test = QUnit.test;
 
 QUnit.module('List template');
 
-function makeListTemplate() {
+function makeListTemplate(champion) {
     const template = document.createElement('template');
     template.innerHTML = `
         <li>
-            <a href="champion-details.html/?id=Aatrox">
-                <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Aatrox.png">
-                <h2>Aatrox</h2>
+            <a href="champion-details.html/?id=${champion.id}">
+                <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${champion.id}.png">
+                <h2>${champion.name}</h2>
             </a>
         </li>
     `;
     return template.content;
 }
 
-test('List template test', assert => {
+test('List template test: Aatrox', assert => {
     // Arrange
     const expected = `
         <li>
@@ -25,9 +25,33 @@ test('List template test', assert => {
             </a>
         </li>
     `;
-
+    const champion = {
+        "id": "Aatrox",
+        "name": "Aatrox",
+    };
     // Act
-    const result = makeListTemplate();
+    const result = makeListTemplate(champion);
+
+    // Assert
+    assert.htmlEqual(result, expected);
+});
+
+test('List template test: Cho\'Gath', assert => {
+    // Arrange
+    const expected = `
+        <li>
+            <a href="champion-details.html/?id=Chogath">
+                <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Chogath.png">
+                <h2>Cho'Gath</h2>
+            </a>
+        </li>
+    `;
+    const champion = {
+        "id": "Chogath",
+        "name": "Cho'Gath",
+    };
+    // Act
+    const result = makeListTemplate(champion);
 
     // Assert
     assert.htmlEqual(result, expected);

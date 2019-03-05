9f37562e8c9bd9ca4dcb5e382629fba07fbc8974
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-04 21:24:28 -0800

    moved details function to separate js started load fn

---
+++ src/queen-details-component.js
@@ -0,0 +1,23 @@
+export function makeQueenDetails(queen) {
+   const template = document.createElement('template');
+   const html = `
+   <dl>
+      <dt>Name:</dt>
+      <dd>${queen.name}</dd>
+      <dt>Quote:</dt>
+      <dd>${queen.quote}</dd>
+      <dt>Winner:</dt>
+      <dd>${queen.winner ? 'Yes' : 'No'}</dd>
+      <dt>Number of Episodes:</dt>
+      <dd>${queen.episodes.length}</dd>
+   </dl>
+   `;
+   template.innerHTML = html;
+   return template.content;
+}
+
+
+
+export default function loadQueenDetails(queen) {
+   
+}
--- tests/make-details.test.js
+++ tests/make-details.test.js
@@ -1,3 +1,4 @@
+import { makeQueenDetails } from '../src/queen-details-component.js';
 const test = QUnit.test;
 
 QUnit.module('Creating details table');
@@ -9,24 +10,6 @@ const testQueen = {
    winner: false,
    episodes: [0, 1, 2, 3, 4, 5, 6, 7],
 };
-
-function makeQueenDetails(queen) {
-   const template = document.createElement('template');
-   const html = `
-   <dl>
-      <dt>Name:</dt>
-      <dd>${queen.name}</dd>
-      <dt>Quote:</dt>
-      <dd>${queen.quote}</dd>
-      <dt>Winner:</dt>
-      <dd>${queen.winner ? 'Yes' : 'No'}</dd>
-      <dt>Number of Episodes:</dt>
-      <dd>${queen.episodes.length}</dd>
-   </dl>
-   `;
-   template.innerHTML = html;
-   return template.content;
-}
 
 test('create DL dynamically with queen object', assert => {
    const expected = `

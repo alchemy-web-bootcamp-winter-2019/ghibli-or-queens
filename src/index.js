9e8533f60b93c0d154aac23bba8013023568dcdc
Ben Parisot <me@benparisot.com>
2019-03-04 18:34:53 -0800

    load movie list on index.html from movie api

--- src/index.js
+++ src/index.js
@@ -1,34 +1,15 @@
-import loadQueenList from './movie-list-component.js/index.js';
+import loadMovieList from './movie-list-component.js';
 
-// const queens = [
-//     {
-//         id: 89,
-//         name: 'Trixie Mattel',
-//         seasons: 3,
-//         quote: 'I love pickles'
-//     },
-//     {
-//         id: 89,
-//         name: 'Trixie Mattel',
-//         seasons: 3,
-//         quote: 'I love pickles'
-//     },
-//     {
-//         id: 89,
-//         name: 'Trixie Mattel',
-//         seasons: 3,
-//         quote: 'I love pickles'
-//     }
-// ];
 
-// const URL = 'http://www.nokeynoshade.party/api/queens/all';
 
-// fetch(URL) 
-//     .then(response => response.json())
-//     .then(queens => {
-//         loadQueenList(queens);
-//     })
-//     .catch(err => {
-//         console.log(err.message);
-//     });
+const URL = 'https://ghibliapi.herokuapp.com/films';
+
+fetch(URL) 
+    .then(response => response.json())
+    .then(movies => {
+        loadMovieList(movies);
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
 

a6a0cd0cf84f5a745a25089077f9166a92aaf2c4
mcnadams <mcneilbo@oregonstate.edu>
2019-03-04 16:29:14 -0800

    move filmDetailTable to module

--- test/film-detail.test.js
+++ test/film-detail.test.js
@@ -1,40 +1,8 @@
+import filmDetailTable from '../src/film-detail.js';
+
 const test = QUnit.test;
 
-
 QUnit.module('build film detail page');
-
-function filmDetailTable(film) {
-    const template = document.createElement('template');
-    template.innerHTML = `
-        <tbody>
-            <tr>
-                <th>Title:</th>
-                <td>${film.title}</td>
-            </tr>
-            <tr>
-                <th>Description:</th>
-                <td>${film.description}</td>
-            </tr>
-            <tr>
-                <th>Director:</th>
-                <td>${film.director}</td>
-            </tr>
-            <tr>
-                <th>Producer:</th>
-                <td>${film.producer}</td>
-            </tr>
-            <tr>
-                <th>Year:</th>
-                <td>${film.release_date}</td>
-            </tr>
-            <tr>
-                <th>Rotten Tomatoes Score:</th>
-                <td>${film.rt_score}</td>
-            </tr>
-        </tbody>
-    `;
-    return template.content;
-}
 
 const film = {
     'id': '2baf70d1-42bb-4437-b551-e5fed5a87abe',

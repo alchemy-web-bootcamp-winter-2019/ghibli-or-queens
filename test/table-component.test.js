0f55a5c62d75ceacf50f0e8da94eb431adbc977f
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-04 16:50:58 -0800

    makeQueenTable passing dynamically

--- test/table-component.test.js
+++ test/table-component.test.js
@@ -2,23 +2,34 @@ const test = QUnit.test;
 
 QUnit.module('Table component');
 
-function makeQueenTable() {
-    return /*html*/`
+function makeQueenTable(queen) {
+    const html = /*html*/`
         <dl>
             <dt>Name:</dt>
-            <dd>Victoria 'Porkchop' Parker</dd>
+            <dd>${queen.name}</dd>
             <dt>Winner:</dt>
-            <dd>No</dd>
+            <dd>${queen.winner ? 'Yes' : 'No'}</dd>
             <dt>Miss Congeniality:</dt>
-            <dd>No</dd>
+            <dd>${queen.missCongeniality ? 'Yes' : 'No'}</dd>
             <dt>Quote:</dt>
-            <dd>Would you fuck me? I'd fuck me.</dd>
+            <dd>${queen.quote}</dd>
         </dl>
     `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
 }
 
 test('make table', function(assert) {
     // arrange
+    const queen = {
+        'id': 1,
+        'name': 'Victoria \'Porkchop\' Parker',
+        'winner': false,
+        'missCongeniality': false,
+        'image_url': 'http://www.nokeynoshade.party/images/victoria-porkchop-parker.jpg',
+        'quote': 'Would you fuck me? I\'d fuck me.'
+    };
     const expected = /*html*/`
         <dl>
             <dt>Name:</dt>
@@ -32,7 +43,7 @@ test('make table', function(assert) {
         </dl>
     `;
     // act
-    const result = makeQueenTable();
+    const result = makeQueenTable(queen);
     // assert
-    assert.equal(result, expected);
+    assert.htmlEqual(result, expected);
 });

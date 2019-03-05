b9e74c3c3d230cad8f09fbbf94a7bc436463a4a3
heather <unphurlng@gmail.com>
2019-03-04 22:54:32 -0800

    detail-template test dynamic GREEN

--- test/detail-template.test.js
+++ test/detail-template.test.js
@@ -2,61 +2,57 @@ const test = QUnit.test;
 
 QUnit.module('DETAIL TEMPLATE TEST');
 
-const movie = /*html*/`
-<dl>
-    <dt class="align-right">Title </dt>
-    <dd class="align-left">Castle in the Sky</dd>
+const movie = {
+    title: 'Castle In The Sky',
+    description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.',
+    director: 'Isao Takahata',
+    release_date: 1988
+};
 
-    <dt class="align-right">Description </dt>
-    <dd class="align-left">"Lorem ipsum dolor sit, amet consectetur adipisicing elit."</dd>
-
-    <dt class="align-right">Director </dt>
-    <dd class="align-left">Isao Takahata</dd>
-
-    <dt class="align-right">Release Date </dt>
-    <dd class="align-left">1988</dd>
-</dl>
-`;
-
-function detailTemplate() {
-    return /*html*/`
+function detailTemplate(movie) {
+    const html = /*html*/`
     <dl>
-        <dt class="align-right">Title </dt>
-        <dd class="align-left">Castle in the Sky</dd>
+        <dt>Title </dt>
+        <dd>${movie.title}</dd>
 
-        <dt class="align-right">Description </dt>
-        <dd class="align-left">"Lorem ipsum dolor sit, amet consectetur adipisicing elit."</dd>
+        <dt>Description </dt>
+        <dd>"${movie.description}"</dd>
 
-        <dt class="align-right">Director </dt>
-        <dd class="align-left">Isao Takahata</dd>
+        <dt>Director </dt>
+        <dd>${movie.director}</dd>
 
-        <dt class="align-right">Release Date </dt>
-        <dd class="align-left">1988</dd>
+        <dt>Release Date </dt>
+        <dd>${movie.release_date}</dd>
     </dl>
     `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    const dom = template.content;
+    return dom;
 }
 
 test('Dynamically create Detail View', assert => {
     // arrange
     const expected = /*html*/`
     <dl>
-        <dt class="align-right">Title </dt>
-        <dd class="align-left">Castle in the Sky</dd>
+        <dt>Title </dt>
+        <dd>Castle In The Sky</dd>
 
-        <dt class="align-right">Description </dt>
-        <dd class="align-left">"Lorem ipsum dolor sit, amet consectetur adipisicing elit."</dd>
+        <dt>Description </dt>
+        <dd>"In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle."</dd>
 
-        <dt class="align-right">Director </dt>
-        <dd class="align-left">Isao Takahata</dd>
+        <dt>Director </dt>
+        <dd>Isao Takahata</dd>
 
-        <dt class="align-right">Release Date </dt>
-        <dd class="align-left">1988</dd>
+        <dt>Release Date </dt>
+        <dd>1988</dd>
     </dl>
     `;
 
     // act
-    const result = detailTemplate();
+    const result = detailTemplate(movie);
 
     // assert
-    assert.equal(result, expected);
+    assert.htmlEqual(result, expected);
 });

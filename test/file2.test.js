7c5dd061beb0e211d6bb34efe66cff4bf77da982
Peter Kotowski <bighoopee@gmail.com>
2019-03-04 19:09:50 -0800

    corrected spacing between html tags after cut n paste to make test pass.

--- test/file2.test.js
+++ test/file2.test.js
@@ -2,27 +2,36 @@ const test = QUnit.test;
 // second html template test
 QUnit.module('movie detail temlate');
 
+function detailPageTemplate(filmName) {
+    const html = /*hml*/`
+<section class="film-info"><dl>Name:</dl><dt>Castle in the Sky</dt><dl>Description</dl><dt>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</dt><dl>Director:</dl><dt>Hayao Miyazaki</dt><dl>Release Date:</dl><dt>1986</dt></section>`;
 
-// copied from expeted
-const html = /*html*/ `
-
-`
-
-
-test('create move detail template', function(assert) {
-    //arrange
-const data = {
-    'object nd key value pairs'
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
 }
-// get data from website object fro description etc
-    //act
-const result = makeAfunction(); // declare above test
-const result = /*htlm*/ `
-`;
 
-//back ticks from html structure
+
+test('create template for detail page', function(assert) {
+    //arrange
+    const filmName = {
+        "title": "Castle in the Sky",
+        "id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+        "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
+        "director": "Hayao Miyazaki",
+        "producer": "Isao Takahata",
+        "release_date": "1986",
+        "rt_score": "95"
+    };
+
+
+    //act
+    const result = detailPageTemplate(filmName);
+    const expected = /*html*/`
+<section class="film-info"><dl>Name:</dl><dt>Castle in the Sky</dt><dl>Description</dl><dt>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</dt><dl>Director:</dl><dt>Hayao Miyazaki</dt><dl>Release Date:</dl><dt>1986</dt></section>`;
+
+
     //assert
 
-
-    assert.equal(true, false);
+    assert.htmlEqual(result, expected)
 });

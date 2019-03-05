d91b28448a9a5a4fee30c6da937b80df2c1d4653
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-04 16:40:51 -0800

    Passing createListItem (dynamically)

--- test/list-component.test.js
+++ test/list-component.test.js
@@ -2,11 +2,11 @@ const test = QUnit.test;
 
 QUnit.module('List component');
 
-function createListItem() {
+function createListItem(queen) {
     const html = /*html*/`
         <li>
-            <img src="http://www.nokeynoshade.party/images/victoria-porkchop-parker.jpg" alt="Image of Victoria 'Porkchop' Parker">
-            <a href="queen-detail.html?id=1">Victoria 'Porkchop' Parker</a>            
+            <img src="${queen.image_url}" alt="Image of ${queen.name}">
+            <a href="queen-detail.html?id=${queen.id}">${queen.name}</a>            
         </li>
     `;
     const template = document.createElement('template');
@@ -22,8 +22,16 @@ test('create list item', function(assert) {
             <a href="queen-detail.html?id=1">Victoria 'Porkchop' Parker</a>            
         </li>
     `;
+    const queen = {
+        'id': 1,
+        'name': 'Victoria \'Porkchop\' Parker',
+        'winner': false,
+        'missCongeniality': false,
+        'image_url': 'http://www.nokeynoshade.party/images/victoria-porkchop-parker.jpg',
+        'quote': 'Would you fuck me? I\'d fuck me.'
+    };
     // act
-    const result = createListItem();
+    const result = createListItem(queen);
     // assert
     assert.htmlEqual(result, expected);
 });

d694146d8163a66b4410f809ad6a92c50ed47fb7
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-03-04 18:07:50 -0700

    created mock data, interpolated function, test green

--- test/movie-list-template.test.js
+++ test/movie-list-template.test.js
@@ -1,9 +1,14 @@
 const test = QUnit.test;
 
-function movieListTemplate() {
+const movie = {
+    title: 'Castle in the Sky',
+    url: 'https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe'
+};
+
+function movieListTemplate(movie) {
     return `
     <li>
-      <a href="google.com">Castle in the Sky</a>
+      <a href="${movie.url}">${movie.title}</a>
     </li>
     `;
 }
@@ -14,11 +19,11 @@ test('display list of movies', assert => {
   //arrange
     const expected = `
     <li>
-      <a href="google.com">Castle in the Sky</a>
+      <a href="https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe">Castle in the Sky</a>
     </li>
     `;
   //act
-    const result = movieListTemplate();
+    const result = movieListTemplate(movie);
   //assert
     assert.equal(result, expected);
 

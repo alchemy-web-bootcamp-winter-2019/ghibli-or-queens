92be564cc3cc557b4e1b17e39da076a9227c77f4
Parker Helland <hellandp@gmail.com>
2019-03-04 17:13:20 -0800

    passed movie list template test dynamically

--- test/template-movie-list.test.js
+++ test/template-movie-list.test.js
@@ -2,20 +2,34 @@ const test = QUnit.test;
 
 QUnit.module('MOVIE LIST TEMPLATE');
 
-function makeMovieListTemplate() {
-    return `
+
+
+const movie = {
+    title: 'Castle in the Sky',
+    url: 'https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe',
+    release_date: 1986
+};
+
+function makeMovieListTemplate(movie) {
+    const html = `
     <li>
-        <a href="https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe">Castle in the Sky (1986)</a>
+        <a href="${ movie.url }">${ movie.title } (${ movie.release_date })</a>
     </li>
     `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    const dom = template.content;
+    return dom;
+
 }
 
 test('movie list template test', assert => {
+    
     const expected = `
     <li>
         <a href="https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe">Castle in the Sky (1986)</a>
     </li>
     `;
-    const result = makeMovieListTemplate();
-    assert.equal(result, expected);
+    const result = makeMovieListTemplate(movie);
+    assert.htmlEqual(result, expected);
 });

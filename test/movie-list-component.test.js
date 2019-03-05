8e089bf4752963e394b51263539a746e79e5931e
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-03-05 08:20:17 -0800

    travis check

--- test/movie-list-component.test.js
+++ test/movie-list-component.test.js
@@ -12,7 +12,7 @@ test('time to write a create dynamic html page', function(assert) {
     //act
     const result = renderHtmlMovieList(movieName);
     const expected = `<li>
-    <a href="./film-details.html/2baf70d1-42bb-4437-b551-e5fed5a87abe">Castle In The Sky</a>
+    <a href="./film-details.html?id=2baf70d1-42bb-4437-b551-e5fed5a87abe">Castle In The Sky</a>
     </li>`;
     //assert
 

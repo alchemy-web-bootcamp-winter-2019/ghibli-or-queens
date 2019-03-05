7f89603cbc4b674bc9bf76976662f61621f18dbb
Parker Helland <hellandp@gmail.com>
2019-03-04 17:16:36 -0800

    built and passed second function test using a different movie title etc

--- test/template-movie-list.test.js
+++ test/template-movie-list.test.js
@@ -2,14 +2,6 @@ const test = QUnit.test;
 
 QUnit.module('MOVIE LIST TEMPLATE');
 
-
-
-const movie = {
-    title: 'Castle in the Sky',
-    url: 'https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe',
-    release_date: 1986
-};
-
 function makeMovieListTemplate(movie) {
     const html = `
     <li>
@@ -20,11 +12,15 @@ function makeMovieListTemplate(movie) {
     template.innerHTML = html;
     const dom = template.content;
     return dom;
-
 }
 
 test('movie list template test', assert => {
-    
+    const movie = {
+        title: 'Castle in the Sky',
+        url: 'https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe',
+        release_date: 1986
+    };
+
     const expected = `
     <li>
         <a href="https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe">Castle in the Sky (1986)</a>
@@ -33,3 +29,22 @@ test('movie list template test', assert => {
     const result = makeMovieListTemplate(movie);
     assert.htmlEqual(result, expected);
 });
+
+test('different title and link for dynamics', assert => {
+    
+    const movie = {
+        title: 'Grave of the Fireflies',
+        url: 'https:/https://ghibliapi.herokuapp.com/films/12cfb892-aac0-4c5b-94af-521852e46d6a',
+        release_date: 1988
+    };
+    
+    const expected = `
+    <li>
+        <a href="https:/https://ghibliapi.herokuapp.com/films/12cfb892-aac0-4c5b-94af-521852e46d6a">Grave of the Fireflies (1988)</a>
+    </li>
+    `;
+    const result = makeMovieListTemplate(movie);
+    assert.htmlEqual(result, expected);
+});
+
+

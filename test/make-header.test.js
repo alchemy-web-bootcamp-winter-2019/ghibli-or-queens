b50ce03e21f136d7fd145ee2dfc28d920a341817
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-05 17:42:21 -0800

    finished header test/passing

--- test/make-header.test.js
+++ test/make-header.test.js
@@ -1,16 +1,17 @@
 const test = QUnit.test;
 
-function makeHeader() {
+function makeHeader(isLink) {
     const html = /*html*/
     `<section id="header-section">
         <img src="assets/logo.png">
         <h1>Studio Ghibli Films</h1>
-        <a id="index-link" href="index.html">Back to All Films</a>
-        </section>`;
+        ${isLink ? `<a id="index-link" href="index.html">Back to All Films</a>
+        </section>` : ''} </section>`;
     const template = document.createElement('template');
     template.innerHTML = html;
     return template.content;
 }
+
 test('make header with link to index.html', assert => {
     const header = /*html*/
     `<section id="header-section">
@@ -20,5 +21,16 @@ test('make header with link to index.html', assert => {
         </section>`;
 
     const result = makeHeader(true);
+    assert.htmlEqual(result, header);
+});
+
+test('make header without link to index.html', assert => {
+    const header = /*html*/
+    `<section id="header-section">
+        <img src="assets/logo.png">
+        <h1>Studio Ghibli Films</h1>
+        </section>`;
+
+    const result = makeHeader(false);
     assert.htmlEqual(result, header);
 });

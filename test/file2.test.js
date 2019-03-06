f25b9e73125cd8beb2f6093fc4c096b78baac0fd
Peter Kotowski <bighoopee@gmail.com>
2019-03-06 00:34:24 -0800

    all tests fixed and passed

--- test/file2.test.js
+++ test/file2.test.js
@@ -21,7 +21,7 @@ test('create template for detail page', function(assert) {
     //act
     const result = detailPageTemplate(filmName);
     const expected = /*html*/`
-<section id ="film-info"><dl>Name:</dl><dt>Castle in the Sky</dt><dl>Description</dl><dt>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</dt><dl>Director:</dl><dt>Hayao Miyazaki</dt><dl>Release Date:</dl><dt>1986</dt></section>`;
+<section id="film-info"> <dl>Name:</dl> <dt>Castle in the Sky</dt> <dl>Description:</dl> <dt>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</dt> <dl>Director:</dl> <dt>Hayao Miyazaki</dt> <dl>Release Date:</dl> <dt>1986</dt></section>`;
 
 
     //assert

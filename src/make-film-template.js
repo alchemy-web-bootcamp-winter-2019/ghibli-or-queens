f37ca4dd4a1ad8f6c7ec5c6054c75e89735f2ce8
Dave Cornelius <d_cornelius94@yahoo.com>
2019-03-07 16:49:55 -0800

    moved film template into own file and created make list test suite

---
+++ src/make-film-template.js
@@ -0,0 +1,21 @@
+export function makeFilmTemplate(film) {
+    const html = /*html*/ `
+    <dl id="film-detail">
+        <dt>Title:</dt>
+        <dd>${film.title}</dd>
+        <dt>Released:</dt>
+        <dd>${film.released}</dd>
+        <dt>Description:</dt>
+        <dd>${film.description}</dd>
+        <dt>Director:</dt>
+        <dd>${film.director}</dd>
+        <dt>Producer:</dt>
+        <dd>${film.producer}</dd>
+        <dt>RT Score:</dt>
+        <dd>${film.rt_score}</dd>
+    </dl>
+    `; 
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
--- test/make-film-template.test.js
+++ test/make-film-template.test.js
@@ -1,3 +1,4 @@
+import { makeFilmTemplate } from '../src/make-film-template.js';
 const test = QUnit.test;
 
 QUnit.module('Making tests for the film template');
@@ -11,27 +12,7 @@ const film = {
     rt_score: 95
 };
 
-function makeFilmTemplate(film) {
-    const html = /*html*/ `
-    <dl id="film-detail">
-        <dt>Title:</dt>
-        <dd>${film.title}</dd>
-        <dt>Released:</dt>
-        <dd>${film.released}</dd>
-        <dt>Description:</dt>
-        <dd>${film.description}</dd>
-        <dt>Director:</dt>
-        <dd>${film.director}</dd>
-        <dt>Producer:</dt>
-        <dd>${film.producer}</dd>
-        <dt>RT Score:</dt>
-        <dd>${film.rt_score}</dd>
-    </dl>
-    `; 
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
+
 
 test('make film list template', assert => {
     //Arrange

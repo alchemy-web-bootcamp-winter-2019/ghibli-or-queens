691e590a40f17b3d81c7d3a6f2aba2d841a3caa4
Parker Helland <hellandp@gmail.com>
2019-03-11 23:04:29 -0700

    movedetail.js running, links not operating properly

--- src/movie-details.js
+++ src/movie-details.js
@@ -1,3 +1,14 @@
 import loadMovieDetails from './details-component.js';
 
-loadMovieDetails();
+const searchParams = new URLSearchParams(window.location.search);
+const id = searchParams.get('id');
+
+if(!id) {
+    window.location = './';
+}
+
+const url = `https://ghibliapi.herokuapp.com/films/${id}`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(result => loadMovieDetails(result));
